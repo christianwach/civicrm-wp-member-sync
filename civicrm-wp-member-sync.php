682b8948db40b93d5b2043b024b1e8c3a3ae363c
Christian Wach <needle@haystack.co.uk>
2023-05-18 22:17:38 +0100

    Bump version to 0.6.0

--- civicrm-wp-member-sync.php
+++ civicrm-wp-member-sync.php
@@ -6,7 +6,7 @@
  * Description: Synchronize CiviCRM Memberships with WordPress User Roles or Capabilities.
  * Author: Christian Wach
  * Author URI: https://haystack.co.uk
- * Version: 0.6.0a
+ * Version: 0.6.0
  * Requires at least: 4.9
  * Requires PHP: 7.1
  * Text Domain: civicrm-wp-member-sync
@@ -35,7 +35,7 @@ if ( ! defined( 'CIVI_WP_MEMBER_SYNC_CAP_PREFIX' ) ) {
 }
 
 // Define plugin version - bumping this will also refresh CSS and JS.
-define( 'CIVI_WP_MEMBER_SYNC_VERSION', '0.6.0a' );
+define( 'CIVI_WP_MEMBER_SYNC_VERSION', '0.6.0' );
 
 // Store reference to this file.
 define( 'CIVI_WP_MEMBER_SYNC_PLUGIN_FILE', __FILE__ );
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: civicrm, member, membership, sync
 Requires at least: 4.9
 Tested up to: 6.2
 Requires PHP: 7.1
-Stable tag: 0.6.0a
+Stable tag: 0.6.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
